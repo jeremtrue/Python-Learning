5db2e148e4a47a2f2a9f63a1e61943391f1a3bf7
jeremtrue <jeremyptruelove7888@gmail.com>
2023-05-12 21:08:32 -0700

    j g

---
+++ gametry.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+
+# init pygame
+pygame.init()
+
+#screen
+WIDTH = 640
+HEIGHT = 480
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#title
+pygame.display.set_caption9("Guess the Number")
+
