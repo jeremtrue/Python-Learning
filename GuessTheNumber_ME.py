e432803b5bb3802074ba270887c93d5774d0b4d5
jeremtrue <jeremyptruelove788@gmail.com>
2023-05-15 16:29:58 -0700

    SHIIMMY SHIMMY *yay* SHIMMAY YAHAHAHAH SHIMMY YAHE

--- GuessTheNumber_ME.py
+++ GuessTheNumber_ME.py
@@ -10,5 +10,8 @@ HEIGHT = 480
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #title
-pygame.display.set_caption9("Guess the Number")
+pygame.display.set_caption("Guess the Number")
+
+#Game variables
+NumberToGuess = random.randint(1, 100)
 
---
+++ RandomNumbers.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random number
+random_number = random.randint(1, 100)
+
+# Output the random number
+print("Random Number:", random_number)
